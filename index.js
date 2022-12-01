2cff0bf87f0fcd727aa3446639c92f178da4fd2b
100ber <76042199+100ber@users.noreply.github.com>
2022-12-01 23:46:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const mongoose =require('mongoose');
+
+const dbString=process.env.DATABASE_URL;
+
+mongoose.connect(dbString);
+
+const db=mongoose.connection;
+db.on('error',(error)=>{(console.log(error))})
+
+db.once('connected',()=>{
+    console.log("Database connected by ss")
+})
+
+const routes = require('./routes/routes');
+
+const app =express();
+
+let cors = require("cors");
+app.use(cors());
+
+app.use(express.json());
+app.use('/backend', routes);
+// app.use('/backend', routes);
+
+
+
+
+app.listen(4000, ()=>{
+    console.log('server started by saurabh')
+})
